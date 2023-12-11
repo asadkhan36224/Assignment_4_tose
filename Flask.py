a9a52a788bb0ec3ecc2545634a9cf74617922216
asadkhan36224 <98026797+asadkhan36224@users.noreply.github.com>
2023-12-11 23:25:48 +0500

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, welcome to my Flask app!'
 
-@app.route('/about')
-def about():
-    return 'This is a simple Flask web application.'
-
-@app.route('/greet/<name>')
-def greet(name):
     return render_template('index.html')
 
 if __name__ == '__main__':
