abd79e98f85f09bfddc1da59cc1657c41b523b9d
asadkhan36224 <98026797+asadkhan36224@users.noreply.github.com>
2023-12-11 23:33:00 +0500

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return ' asad has comeback!'
 
-    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
