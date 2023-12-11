053038140d6023db6477c315e26c9c7a9a6fe616
asadkhan36224 <98026797+asadkhan36224@users.noreply.github.com>
2023-12-11 22:17:43 +0500

    Add files via upload

---
+++ Flask.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, welcome to my Flask app!'
+
+@app.route('/about')
+def about():
+    return 'This is a simple Flask web application.'
+
+@app.route('/greet/<name>')
+def greet(name):
+    return render_template('greet.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
